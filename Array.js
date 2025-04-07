a4fd83915da14daa7eab5fa0255c2e12321f72b0
user <user@users-MacBook-Pro.local>
2025-04-07 21:05:42 +0100

    Merge Sorted Array

--- Array.js
+++ Array.js
@@ -1,3 +1,4 @@
+// Question 1
 // Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
 
 // Note that elements beyond the length of the original array are not written.
@@ -20,3 +21,28 @@ var duplicateZeros = function (arr) {
   return arr;
 };
 console.log(duplicatesZeros([1, 0, 2, 3, 0, 4, 5, 0])); //[0,0,4,1,0,0,0,0,8][1,0,0,2,3,0,0,4]
+
+// Question 2
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+// To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored.
+// nums2 has a length of n.
+Input: (nums1 = [1, 2, 3, 0, 0, 0]), (m = 3), (nums2 = [2, 5, 6]), (n = 3);
+Output: [1, 2, 2, 3, 5, 6];
+Input: (nums1 = [1]), (m = 1), (nums2 = []), (n = 0);
+Output: [1];
+Input: (nums1 = [0]), (m = 0), (nums2 = [1]), (n = 1);
+Output: [1];
+// Solution :
+var merge = function (nums1, m, nums2, n) {
+  let negative = -n;
+  nums1.splice(negative, n, ...nums2);
+
+  return nums1.sort((a, b) => a - b);
+};
+console.log(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)); //[-1,0,0,1,2,2,3,3,3]
+console.log(merge([1, 0], 1, [2], 1)); //[1,2]
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); //[1,2,2,3,5,6]
