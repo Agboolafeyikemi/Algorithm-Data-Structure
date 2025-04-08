099b65b6f9c3758d7e7a1d602327aada8313dd87
user <user@users-MacBook-Pro.local>
2025-04-08 22:13:53 +0100

    Find All Numbers Disappeared in an Array

--- Array.js
+++ Array.js
@@ -94,3 +94,17 @@ var sortArrayByParity = function (nums) {
 // Example 2:
 // Input: nums = [1,1]
 // Output: [2]
+// Solution
+var findDisappearedNumbers = function (nums) {
+  const numSet = new Set(nums);
+  const result = [];
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!numSet.has(i)) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); //[5,6], //[2]
